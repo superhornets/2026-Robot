6bcdfd6a6a5a76f06ffa521599233ab20aaf395b
Ian <ian@nowhwere.com>
2026-01-08 19:06:30 -0600

    feature/autos: finished attempt to get autobuilder configured

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
 
@@ -170,6 +171,17 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     public Pose2d getPose() {
         return getState().Pose;
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds() {
+        return getState().Speeds;
+    }
+
+    public void driveRobotRelative(ChassisSpeeds chassisSpeeds) {
+        double x = chassisSpeeds.vxMetersPerSecond / DriveConstants.kMaxSpeedMetersPerSecond;
+        double y = chassisSpeeds.vyMetersPerSecond / DriveConstants.kMaxSpeedMetersPerSecond;
+        double omega = chassisSpeeds.omegaRadiansPerSecond / (Math.PI * 2);
+        drive(x, y, omega, false, true);
     }
 
     /**
