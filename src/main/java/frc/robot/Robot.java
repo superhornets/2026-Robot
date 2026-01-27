4943d96affe49c299794e3cb61604120185b3590
Josh Monroe <5832+jmonroe@users.noreply.github.com>
2026-01-26 21:29:03 -0600

    Working on pathfinding

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import frc.robot.Commands.OrchestraPlayCommand;
+import frc.robot.Commands.OrchestraStopCommand;
+
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
@@ -55,13 +58,17 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    
+    m_robotContainer.m_orchestra.play();
   }
 
   @Override
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {}
+  public void teleopExit() {
+    m_robotContainer.m_orchestra.stop();
+  }
 
   @Override
   public void testInit() {
