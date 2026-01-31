4b94f061e1d649ad9bbb546460d9f0644ef75b6b
Sophia <173519141+Sophiaphillips414@users.noreply.github.com>
2026-01-31 09:45:41 -0600

    changed gear ratio

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -80,7 +80,7 @@ public class TunerConstants {
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 0;
 
-    private static final double kDriveGearRatio = 17.5;
+    private static final double kDriveGearRatio = 6;
     private static final double kSteerGearRatio = 25;
     private static final Distance kWheelRadius = Inches.of(2);
 
