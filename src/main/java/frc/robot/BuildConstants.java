4861a2e6e51ec942af849070f2130c0f13e57231
SparkMax <team@superhornets.org>
2026-02-07 14:09:20 -0600

    added empty subsystems

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 52;
-  public static final String GIT_SHA = "ae455d2c1a98d06f45688dc767a350fafd4bad58";
-  public static final String GIT_DATE = "2026-02-07 11:54:26 EST";
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "659552e772f8bc49f3c1b317ecec67b4de8f644b";
+  public static final String GIT_DATE = "2026-02-07 13:22:08 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-07 13:05:52 EST";
-  public static final long BUILD_UNIX_TIME = 1770487552584L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-07 13:23:16 EST";
+  public static final long BUILD_UNIX_TIME = 1770488596989L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
