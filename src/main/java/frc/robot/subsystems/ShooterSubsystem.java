8666d4c51491dcf706983c0280a94ac388a60051
SparkMax <team@superhornets.org>
2026-02-07 15:29:56 -0600

    added maybe broken code for sparkMax motor controller to ShooterSubsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,14 +4,93 @@
 
 package frc.robot.subsystems;
 
+import java.lang.ProcessBuilder.Redirect.Type;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+  public SparkMax motor;
+  public SparkMaxConfig motorConfig;
+  public SparkClosedLoopController controller;
+  public RelativeEncoder encoder;
+
+
+
+
   /** Creates a new ShooterSubsystem. */
-  public ShooterSubsystem() {}
+  public ShooterSubsystem() {
+    motor = new SparkMax(0000000000000000000, MotorType.kBrushless);
+    controller = motor.getClosedLoopController();
+    encoder = motor.getEncoder();
+    motorConfig.encoder 
+      .positionConversionFactor(1)
+      .velocityConversionFactor(1);
+    
+    motorConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // Set PID values for position control. We don't need to pass a closed loop
+        // slot, as it will default to slot 0.
+        .p(0.1)
+        .i(0)
+        .d(0)
+        .outputRange(-1, 1)
+        // Set PID values for velocity control in slot 1
+        .p(0.0001, ClosedLoopSlot.kSlot1)
+        .i(0, ClosedLoopSlot.kSlot1)
+        .d(0, ClosedLoopSlot.kSlot1)
+        .outputRange(-1, 1, ClosedLoopSlot.kSlot1)
+        .feedForward
+          // kV is now in Volts, so we multiply by the nominal voltage (12V)
+          .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
+
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    SmartDashboard.setDefaultNumber("Target Position", 0);
+    SmartDashboard.setDefaultNumber("Target Velocity", 0);
+    SmartDashboard.setDefaultBoolean("Control Mode", false);
+    SmartDashboard.setDefaultBoolean("Reset Encoder", false);
+  }
+
+  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (SmartDashboard.getBoolean("Control Mode", false)) {
+      /*
+       * Get the target velocity from SmartDashboard and set it as the setpoint
+       * for the closed loop controller.
+       */
+      double targetVelocity = SmartDashboard.getNumber("Target Velocity", 0);
+      controller.setSetpoint(targetVelocity, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
+    } else {
+      /*
+       * Get the target position from SmartDashboard and set it as the setpoint
+       * for the closed loop controller.
+       */
+      double targetPosition = SmartDashboard.getNumber("Target Position", 0);
+      controller.setSetpoint(targetPosition, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    }
+
+    SmartDashboard.putNumber("Actual Position", encoder.getPosition());
+    SmartDashboard.putNumber("Actual Velocity", encoder.getVelocity());
+
+    if (SmartDashboard.getBoolean("Reset Encoder", false)) {
+      SmartDashboard.putBoolean("Reset Encoder", false);
+      // Reset the encoder position to 0
+      encoder.setPosition(0);
+    }
   }
 }
