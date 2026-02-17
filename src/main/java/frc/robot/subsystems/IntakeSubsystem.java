69fc6aa360af2ebe73b1c5b40dadee9bbb4d52d7
Josh Monroe <monroe.josh@gmail.com>
2026-02-16 20:41:07 -0600

    SimGUI updates

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -39,6 +39,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    leftIntake.simulationPeriodic();
+    rightIntake.simulationPeriodic();
   }
 }
