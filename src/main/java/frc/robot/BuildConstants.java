c54bc8854ce49fb8b50f1b09b5218321d2e68ebd
Josh Monroe <5832+jmonroe@users.noreply.github.com>
2026-02-17 21:10:15 -0600

    Updating Tuner Constants for the new drive base

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "69fc6aa360af2ebe73b1c5b40dadee9bbb4d52d7";
-  public static final String GIT_DATE = "2026-02-16 21:41:07 EST";
+  public static final int GIT_REVISION = 70;
+  public static final String GIT_SHA = "fab14481c7a39267a4d618587da9d7cf12f46e7b";
+  public static final String GIT_DATE = "2026-02-16 22:15:38 EST";
   public static final String GIT_BRANCH = "IntakeSubsystem";
-  public static final String BUILD_DATE = "2026-02-16 22:12:11 EST";
-  public static final long BUILD_UNIX_TIME = 1771297931074L;
+  public static final String BUILD_DATE = "2026-02-17 21:48:25 EST";
+  public static final long BUILD_UNIX_TIME = 1771382905341L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
