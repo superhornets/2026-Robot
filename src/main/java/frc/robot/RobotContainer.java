0b52be7a95877a31b473ef070a23305be52bbf13
Sophiaphillips414 <173519141+Sophiaphillips414@users.noreply.github.com>
2026-03-09 19:45:16 -0500

    added import for climber commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Commands.ClimberCommands;
 import frc.robot.Commands.DriveCommands;
 import frc.robot.Commands.IntakeCommands;
 import frc.robot.Commands.PathCommands;
