46c8745259b153786d3eb6018f8b5b078d45230e
Josh Monroe <monroe.josh@gmail.com>
2026-03-21 12:06:18 -0500

    possiby finaly getting the current limits right

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -61,8 +61,8 @@ public class TunerConstants {
   private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
     .withCurrentLimits(
         new CurrentLimitsConfigs()
-        .withStatorCurrentLimit(Amps.of(20))
-        .withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(Amps.of(10))
+        .withStatorCurrentLimitEnable(true)
     );
   private static final TalonFXConfiguration steerInitialConfigs =
       new TalonFXConfiguration()
@@ -71,7 +71,7 @@ public class TunerConstants {
                   // Swerve azimuth does not require much torque output, so we can set a relatively
                   // low
                   // stator current limit to help avoid brownouts without impacting performance.
-                  .withStatorCurrentLimit(Amps.of(10))
+                  .withStatorCurrentLimit(Amps.of(5))
                   .withStatorCurrentLimitEnable(true));
   private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
   // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
