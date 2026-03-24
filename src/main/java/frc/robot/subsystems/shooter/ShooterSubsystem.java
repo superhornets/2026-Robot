07f96beae1adbf2aaf7cbe91d734e1b548e9f96e
Josh Monroe <monroe.josh@gmail.com>
2026-03-23 19:59:39 -0500

    tuning options for elastic dashboard. current limits for shooter flywheel

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -101,6 +101,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
   SparkFlexConfig flywheelConfig = new SparkFlexConfig();
     flywheelConfig
+        .smartCurrentLimit(20, 40, 50)
         .closedLoop
         .p(0.0001)
         .i(0)
